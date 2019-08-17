8062020a4b13d973fe7bac210cdf2e41652f69a7
Thiago <thiagoseronni@gmail.com>
2019-08-17 18:38:59 -0300

    1.2

--- app_gp/utils/utils.py
+++ app_gp/utils/utils.py
@@ -1,8 +1,9 @@
 from django.utils.text import slugify
+from PIL import Image
+import os
 
 
 def unique_slug_generator(instance, counter=0):
-
     slug = slugify(instance.fake_name)
 
     if counter > 0:
@@ -13,6 +14,69 @@ def unique_slug_generator(instance, counter=0):
     qs_exists = klass.objects.filter(slug=slug).exclude(id=instance.id).exists()
 
     if qs_exists:
-        return unique_slug_generator(instance, counter=counter+1)
+        return unique_slug_generator(instance, counter=counter + 1)
 
     return slug
+
+
+def images_resize_path(from_path='',
+                       to_path='',
+                       square_fit_size=300,
+                       prefix_name='image',
+                       save_as_jpg=True):
+    if save_as_jpg:
+        ext = 'jpg'
+    else:
+        ext = 'png'
+
+    image_number = 1
+    for root, dirs, files in os.walk(from_path):
+        for filename in files:
+            if filename.endswith('.png') or filename.endswith('.jpg'):
+                filename = os.path.join(root, filename)
+                im = Image.open(filename)
+                width, height = im.size
+
+                if width > height:
+                    height = int((square_fit_size / width) * height)
+                    width = square_fit_size
+                else:
+                    width = int((square_fit_size / height) * width)
+                    height = square_fit_size
+
+                # Redimensiona a imagem
+                im = im.resize((width, height))
+
+                save_path_name = os.path.join(to_path, f'{prefix_name}_{image_number}.{ext}')
+                image_number = image_number + 1
+
+                im.save(save_path_name)
+
+
+def image_resize(image_path='',
+                 to_path='',
+                 square_fit_size=300,
+                 prefix_name='image',
+                 postfix_name='thumb',
+                 save_as_jpg=True):
+    if save_as_jpg:
+        ext = 'jpg'
+    else:
+        ext = 'png'
+
+    im = Image.open(image_path)
+    width, height = im.size
+
+    if width > height:
+        height = int((square_fit_size / width) * height)
+        width = square_fit_size
+    else:
+        width = int((square_fit_size / height) * width)
+        height = square_fit_size
+
+    # Redimensiona a imagem
+    im = im.resize((width, height))
+
+    save_path_name = os.path.join(to_path, f'{prefix_name}_{postfix_name}.{ext}')
+
+    im.save(save_path_name)
