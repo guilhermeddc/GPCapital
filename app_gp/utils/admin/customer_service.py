27add2ae9abbf2a6c201ff6c35ac68ab8a6fc593
Thiago <thiagoseronni@gmail.com>
2019-08-20 19:44:31 -0300

    1.4

--- app_gp/utils/admin/customer_service.py
+++ app_gp/utils/admin/customer_service.py
@@ -7,13 +7,6 @@ class TabularClientCustomerServices(admin.TabularInline):
     model = InterClientCustomerServices
     exclude = ()
 
-    # def formfield_for_dbfield(self, db_field, **kwargs):
-    #     attrs = {'size': 15}
-    #     if db_field.attname == 'interest_level':
-    #         attrs = {'size': 2}
-    #     kwargs['widget'] = widgets.CheckboxSelectMultiple()
-    #     return super(AdminInlineCustomerServices, self).formfield_for_dbfield(db_field, **kwargs)
-
 
 class ModelFormCustomerService(ModelForm):
 
