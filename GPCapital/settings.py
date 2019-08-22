a1237bac9aa6e600d75508e884f360eb735d028c
Guilherme <guilhermeddc@gmail.com>
2019-08-22 17:58:45 -0300

    1

--- GPCapital/settings.py
+++ GPCapital/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,26 +124,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 # In the INSTALLED_APPS the 'django.contrib.staticfiles' will serve all static files and the URL tag will begin with
 # STATIC_URL
-# STATIC_URL = '/static/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # That's where files get collected automatically after you run manage.py collectstatic
 # This will export all static files stored in $APP/static/ and STATICFILES_DIRS to STATIC_ROOT
@@ -157,12 +144,8 @@ STATICFILES_DIRS = (
 # ]
 
 # Heroku config
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
 
 MEDIA_URL = '/media/'
-
 
 if os.getcwd() == '/app':
     import dj_database_url
