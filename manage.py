bb01aafa84c48cb32a474e555e8365e74580e2ba
Thiago <thiagoseronni@gmail.com>
2019-08-27 20:45:51 -0300

    add some files and configs to deploy on amazon

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPCapital.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPCapital.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
