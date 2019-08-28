f30f4a4fc3cf6e0cdd7bd20304633930737b3938
Thiago <thiagoseronni@gmail.com>
2019-08-27 22:24:30 -0300

    add some files and configs to deploy on amazon

--- app_gp/management/commands/cm_populate_initial.py
+++ app_gp/management/commands/cm_populate_initial.py
@@ -160,4 +160,47 @@ class Command(BaseCommand):
         for obj in choices_services_offered:
             ChoicesServicesOffered.objects.create(services=obj)
 
+        choices_states = [
+            ["AC", "Acre", 12],
+            ["AL", "Alagoas", 27],
+            ["AM", "Amazonas", 13],
+            ["AP", "Amapá", 16],
+            ["BA", "Bahia", 29],
+            ["CE", "Ceará", 23],
+            ["DF", "Distrito Federal", 53],
+            ["ES", "Espírito Santo", 32],
+            ["GO", "Goiás", 52],
+            ["MA", "Maranhão", 21],
+            ["MG", "Minas Gerais", 31],
+            ["MS", "Mato Grosso do Sul", 50],
+            ["MT", "Mato Grosso", 51],
+            ["PA", "Pará", 15],
+            ["PB", "Paraíba", 25],
+            ["PE", "Pernambuco", 26],
+            ["PI", "Piauí", 22],
+            ["PR", "Paraná", 41],
+            ["RJ", "Rio de Janeiro", 33],
+            ["RN", "Rio Grande do Norte", 24],
+            ["RO", "Rondônia", 11],
+            ["RR", "Roraima", 14],
+            ["RS", "Rio Grande do Sul", 43],
+            ["SC", "Santa Catarina", 42],
+            ["SE", "Sergipe", 28],
+            ["SP", "São Paulo", 35],
+            ["TO", "Tocantins", 17]
+        ]
+
+        for obj in choices_states:
+            ChoicesStates.objects.create(uf=obj[0], state=obj[1], ibge_code=obj[2])
+
+        choices_status = [
+            "Ativo",
+            "Em aprovação",
+            "Aguardando pagamento",
+            "Desativado",
+            "Reprovado",
+        ]
+        for obj in choices_status:
+            ChoicesStatus.objects.create(status=obj)
+
         self.stdout.write(self.style.SUCCESS('INSERTED!'))
