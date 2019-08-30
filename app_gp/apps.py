685566898d7c1694592747e467ff4b32c9eab6ab
Thiago <thiagoseronni@gmail.com>
2019-08-30 15:35:50 -0300

    some changes

--- app_gp/apps.py
+++ app_gp/apps.py
@@ -5,6 +5,6 @@ class AppGpConfig(AppConfig):
     name = 'app_gp'
 
     def ready(self):
-        from app_gp.signals import h_choicescity, h_choicesgenre, h_client, h_clientphoto, h_clientvideo
+        from app_gp.signals import h_choicescity, h_choicesgenre, h_client, h_clientphoto, h_clientvideo, h_highlight
 
 
